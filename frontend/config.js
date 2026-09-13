532538626cfc935c6e67503472e9d763ee040fff
ipsrs-rspc-91 <herry.cpt@gmail.com>
2026-09-13 16:52:12 +0700

    Update IPSRS_API_URL to production endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Isi URL Web App Apps Script setelah deployment. Jangan masukkan credential/token di sini.
-window.IPSRS_API_URL = 'GANTI_DENGAN_URL_WEB_APP_APPS_SCRIPT';
+window.IPSRS_API_URL = 'https://script.google.com/macros/s/AKfycbwaYIWpW0yw-Qr_kFtQS_SdI31e9Tom1TfMyON616m2XTV_QEZGXrnyH44HNDwHivcI/exec';
