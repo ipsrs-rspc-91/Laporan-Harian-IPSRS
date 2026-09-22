22b84c66914c02e75ad889feaf5989b3aa854cfc
ipsrs-rspc-91 <herry.cpt@gmail.com>
2026-09-23 06:34:55 +0700

    Keep API URL compatibility helper for auxiliary UI scripts

--- frontend/app.js
+++ frontend/app.js
@@ -144,6 +144,7 @@
     return window.IPSRS_SUPABASE_CLIENT;
   }
 
+  function getApiUrl(){ return window.IPSRS_SUPABASE_API_URL || ''; }
   function legacyLoginUrl_(){ return window.IPSRS_API_URL || 'https://script.google.com/macros/s/AKfycbzC7caqnB_sDXgNV5sW1nKbl4m2JM22qK-zWIx_VQnI4I2icZcLldatvq9UsqMEHf1Z/exec'; }
 
   function staffAuthEmail_(staffId){
