ebd38e44581d7be484e5c5a6e8c87812e87c78f7
ipsrs-rspc-91 <herry.cpt@gmail.com>
2026-09-23 12:57:25 +0700

    Remove legacy migration endpoints from frontend config

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,4 @@
 window.IPSRS_SUPABASE_URL = 'https://tcrmlhfsroyhaxdfwyll.supabase.co';
 window.IPSRS_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_TtDuzNo7h_668fq_7x4Sog_ZvYcFgmq';
 window.IPSRS_SUPABASE_API_URL = window.IPSRS_SUPABASE_URL + '/functions/v1/ipsrs-api';
-window.IPSRS_SUPABASE_BOOTSTRAP_URL = window.IPSRS_SUPABASE_URL + '/functions/v1/ipsrs-legacy-bootstrap';
-window.IPSRS_SUPABASE_LINK_URL = window.IPSRS_SUPABASE_URL + '/functions/v1/ipsrs-link-account';
-window.IPSRS_SUPABASE_AUTH_BRIDGE_URL = window.IPSRS_SUPABASE_URL + '/functions/v1/ipsrs-auth-bridge';
 window.IPSRS_SUPABASE_EMAIL_DOMAIN = '@auth.ipsrs.local';
